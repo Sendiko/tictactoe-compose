c87d54c3e3749e4b9d8eee5bca32a71b4035d583
Sendiko <sarangtawon897@gmail.com>
2023-09-21 11:13:57 +0700

    enhancement - create MatchData

---
+++ app/src/main/java/com/sendiko/composetictactoe/ui/screens/history/MatchData.kt
@@ -0,0 +1,8 @@
+package com.sendiko.composetictactoe.ui.screens.history
+
+data class MatchData(
+    val playerOne: String,
+    val playerOneScore: Int,
+    val playerTwo: String,
+    val playerTwoScore: Int,
+)
